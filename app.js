070c775e859c5a45f7d53b964efd29b881921856
aber866 <aber866@users.noreply.github.com>
2016-05-03 17:16:41 +0100

    Upload components

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+ * Module dependencies
+ */
+
+import React from 'react';
+import PokeTable from './components/PokeTable';
+
+
+React.render(
+  <PokeTable />,
+  document.getElementById('container')
+);
---
+++ commands.md
@@ -0,0 +1,4 @@
+npm install --save react
+npm install --save browserify
+npm install --save babelify
+npm run build
